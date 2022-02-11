40b61b63bc15e245cf337d1a1832b704192d5821
Zi-ji <zhuzhijie1003@outlook.com>
2022-02-11 23:18:46 +1100

    home view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
 import type { NextPage } from 'next'
+import HomeView from '../views/HomeView'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="h-screen w-screen">
       <Head>
         <title>logicpop</title>
         <meta name="description" content="Logicpop website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>logicpop</main>
+      <HomeView />
 
       <footer></footer>
     </div>
---
+++ src/views/HomeView.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomeView() {
+  return (
+    <div className="view-container">
+      <h1 className="text-3xl font-medium leading-loose">hello, we are</h1>
+      <h1 className="text-6xl text-primary font-medium">logicpop</h1>
+    </div>
+  )
+}
+
+export default HomeView
