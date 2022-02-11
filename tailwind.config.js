1258178702b5ac0261c2a897e53272f144570239
Zi-ji <zhuzhijie1003@outlook.com>
2022-02-11 22:33:08 +1100

    basic tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#1F93FF',
+        focused: '#000000', // text color
+        unfocused: '#999999', // eg. unselected menu link
+        label: '#808080',
+        'bg-card': '#f8f8f8',
+        'bg-wash': '#abc8ff'
+      }
+    }
   },
   plugins: []
 }
