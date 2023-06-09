c6b565134f87c2f9b65116526bf1ac8bb040e8d9
Eric Liu <me@fxlui.com>
2023-06-09 17:08:53 +1000

    update richard description

--- src/views/TeamView.tsx
+++ src/views/TeamView.tsx
@@ -9,7 +9,7 @@ function TeamView() {
     <div className="flex flex-col lg:flex-row justify-center mt-8 md:mt-0">
       <TeamCard name="James Wong" photo={james} description="business" />
       <TeamCard name="Eric Liu" photo={eric} description="design" />
-      <TeamCard name="Richard Zhu" photo={richard} description="technical" />
+      <TeamCard name="Richard Zhu" photo={richard} description="technology" />
     </div>
   )
 }
